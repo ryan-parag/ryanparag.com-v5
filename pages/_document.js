42789981b22ca0597abe7ec01a219ec61d62c0b6
ryan-parag <parag.ryan@gmail.com>
2023-09-30 19:09:41 -0400

    head

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,24 @@ export default function Document() {
 
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+        <link rel="icon" type="image/png" href="/favicon.png"/>
+        <title>Ryan Parag</title>
+        <meta name="title" content="Ryan Parag"/>
+        <meta name="description" content="Product designer living in Tampa, FL"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://ryanparag.com/"/>
+        <meta property="og:title" content="Ryan Parag"/>
+        <meta property="og:description" content="Product designer living in Tampa, FL"/>
+        <meta property="og:image" content="/social-media.png"/>
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://ryanparag.com/"/>
+        <meta property="twitter:title" content="Ryan Parag"/>
+        <meta property="twitter:description" content="Product designer living in Tampa, FL"/>
+        <meta property="twitter:image" content="/social-media.png"/>
+        <meta name="keywords" content="ryan, parag, graphic, web, designer, product, tampa, design, trustlayer, masonite, chargebacks911"></meta>
+      </Head>
       <body id="body">
         <div
           className="transition absolute top-0 left-0 right-0 overflow-x-hidden h-96 pointer-events-none z-0"
