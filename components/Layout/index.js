621a211e2811431aff8321dc30885a210580fac2
Ryan Parag <parag.ryan@gmail.com>
2025-10-30 08:01:07 -0400

    fix head and og

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -47,32 +47,10 @@ const Layout = ({ children }) => {
     updateLocalStorage(currentColor)
   }, [currentColor, darkMode]);
 
-  const meta = {
-    name: 'Ryan Parag',
-    description: 'Ryan Parag is a product designer and design engineer helping build systems and products.',
-    url: 'https://ryanparag.com'
-  }
-
   return(
     <>
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
-        <link rel="icon" type="image/png" href="/favicon.png"/>
-        <title>Ryan Parag</title>
-        <meta name="title" content={meta.name}/>
-        <meta name="description" content={meta.description}/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content={meta.url}/>
-        <meta property="og:title" content={meta.name}/>
-        <meta property="og:description" content={meta.description}/>
-        <meta property="og:image" content="/social-media.png"/>
-        <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content={meta.url}/>
-        <meta property="twitter:title" content={meta.name}/>
-        <meta property="twitter:description" content={meta.description}/>
-        <meta property="twitter:image" content="/social-media.png"/>
-        <meta name="keywords" content="ryan, parag, graphic, web, designer, product, tampa, design, trustlayer, masonite, chargebacks911"/>
-        <meta name="robots" content="index, follow"/>
+        <title>Ryan Parag — Product Designer</title>
       </Head>
       <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`} />
       <Script id="google-analytics">
--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,32 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 
+  const meta = {
+    name: 'Ryan Parag',
+    description: 'Ryan Parag is a product designer and design engineer helping build systems and products.',
+    url: 'https://ryanparag.com'
+  }
+
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
+        <link rel="icon" type="image/png" href="/favicon.png"/>
+        <meta name="title" content={meta.name}/>
+        <meta name="description" content={meta.description}/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content={meta.url}/>
+        <meta property="og:title" content={meta.name}/>
+        <meta property="og:description" content={meta.description}/>
+        <meta property="og:image" content="/social-media.png"/>
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content={meta.url}/>
+        <meta property="twitter:title" content={meta.name}/>
+        <meta property="twitter:description" content={meta.description}/>
+        <meta property="twitter:image" content="/social-media.png"/>
+        <meta name="keywords" content="ryan, parag, graphic, web, designer, product, tampa, design, trustlayer, masonite, chargebacks911"/>
+        <meta name="robots" content="index, follow"/>
+      </Head>
       <body id="body">
         <Main />
         <div
