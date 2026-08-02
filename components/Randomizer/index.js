13ff6c54e4ad8c3526f3c307302953ea204fb249
Ryan Parag <parag.ryan@gmail.com>
2026-08-02 10:33:00 -0400

    add loader to randomizer

--- components/Randomizer/index.js
+++ components/Randomizer/index.js
@@ -1,5 +1,31 @@
 import React, { useState, useEffect } from 'react'
-import { TextMorph } from 'torph/react';
+
+const Spinner = () => {
+  return(
+    <svg
+      className="inline-flex w-6 h-6 animate-spin text-themePrimary"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  )
+}
 
 const Randomizer = () => {
 
@@ -77,21 +103,28 @@ const Randomizer = () => {
   }
 
   const [isRandom, setIsRandom] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     setIsRandom(randomFact())
   }, [])
 
+  const handleClick = () => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsRandom(randomFact())
+      setIsLoading(false)
+    }, 320)
+  }
+
   return(
       <span
         role="button"
         className="link-shadow select-none"
-        onClick={() => setIsRandom(randomFact())}
+        onClick={handleClick}
         title={`Cycle randomly through all ${facts.length} fun facts!`}
       >
-        <TextMorph>
-          {isRandom.text}
-        </TextMorph>
+        {isLoading ? <Spinner/> : isRandom.text }
       </span>
   )
 }
